8e113c8e24cc3d1c490c3c93310ef412d20e4bec
Ubuntu <ubuntu@ip-172-31-33-136.ap-south-1.compute.internal>
2025-01-22 17:03:37 +0000

    docker changes

--- Dockerfile
+++ Dockerfile
@@ -7,11 +7,11 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 
-ENV DB_NAME=myproject
-ENV DB_USER=myuser
-ENV DB_PASSWORD=mypassword
-ENV DB_HOST=postgres-service
-ENV DB_PORT=5432
+#ENV DB_NAME=myproject
+#ENV DB_USER=myuser
+#ENV DB_PASSWORD=mypassword
+#ENV DB_HOST=postgres-service
+#ENV DB_PORT=5432
 
 EXPOSE 8000
 CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- travel/settings.py
+++ travel/settings.py
@@ -75,18 +75,18 @@ WSGI_APPLICATION = 'travel.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'myproject'),
-        'USER': os.environ.get('DB_USER', 'myuser'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'mypassword'),
-        'HOST': os.environ.get('DB_HOST', 'postgres-service'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': os.environ.get('DB_NAME', 'myproject'),
+#        'USER': os.environ.get('DB_USER', 'myuser'),
+#        'PASSWORD': os.environ.get('DB_PASSWORD', 'mypassword'),
+#        'HOST': os.environ.get('DB_HOST', 'postgres-service'),
+#        'PORT': os.environ.get('DB_PORT', '5432'),
+#    }
 }
 
 
